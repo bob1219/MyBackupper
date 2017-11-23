358d9335b3676a1cbb6ad8ed004d83f014e2fd7c
吉田大輝 <bob181219@yahoo.co.jp>
2017-11-23 16:47:25 +0900

    Support directory backup

--- backup.c
+++ backup.c
@@ -1,6 +1,8 @@
 /* Standard Libraries */
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
 
 /* Headers */
 #include "macros.h"
@@ -37,21 +39,70 @@ backup
 		if(from	[strlen(from) - 1] == '\n')from	[strlen(from) - 1] = '\0';
 		if(to	[strlen(to	) - 1] == '\n')to	[strlen(to	) - 1] = '\0';
 		
-		/* Open file */
-		BackupFromFilePointer	= fopen(from,	"rb");
-		BackupToFilePointer		= fopen(to,		"wb");
-		if((BackupFromFilePointer == NULL) || (BackupToFilePointer == NULL))
+		if(from[strlen(from) - 1] == PATH_BREAK_CHARACTER)
 		{
-			fprintf(stderr, "Error: Cannot open file.\n");
-			continue;
-		}
-		
-		while(1)
-			if((fread(&b, sizeof(BYTE), 1, BackupFromFilePointer) < 1) || (fwrite(&b, sizeof(BYTE), 1, BackupToFilePointer) < 1))break;
+			DIR				*directory;
+			struct dirent	*DirectoryPointer;
+			char			from2[FILENAME_MAX], to2[FILENAME_MAX];
+			size_t			FilenameLength;
 			
-		/* Close file */
-		fclose(BackupFromFilePointer);
-		fclose(BackupToFilePointer);
+			/* Open directory */
+			directory = opendir(from);
+			if(directory == NULL)
+			{
+				fprintf(stderr, "Error: Cannot open directory \"%s\".\n", from);
+				continue;
+			}
+			
+			for(DirectoryPointer = readdir(directory) ; DirectoryPointer != NULL ; DirectoryPointer = readdir(directory))
+			{
+				if((strcmp(DirectoryPointer -> d_name, ".") != 0) && (strcmp(DirectoryPointer -> d_name, "..") != 0))
+				{
+					FilenameLength = strlen(DirectoryPointer -> d_name);
+					if(((strlen(from) + FilenameLength + 1) > FILENAME_MAX) || ((strlen(to) + FilenameLength + 1) > FILENAME_MAX))abort();
+					
+					/* Input path of "from" file to "from2" */
+					sprintf(from2, "%s%s", from, DirectoryPointer -> d_name);
+					
+					/* Input path of "to" file to "to2" */
+					sprintf(to2, "%s%s", to, DirectoryPointer -> d_name);
+					
+					/* Open file */
+					BackupFromFilePointer	= fopen(from2,	"rb");
+					BackupToFilePointer		= fopen(to2,	"wb");
+					if((BackupFromFilePointer == NULL) || (BackupToFilePointer == NULL))
+					{
+						fprintf(stderr, "Error: Cannot open file \"%s\".\n", DirectoryPointer -> d_name);
+						continue;
+					}
+		
+					while(1)
+						if((fread(&b, sizeof(BYTE), 1, BackupFromFilePointer) < 1) || (fwrite(&b, sizeof(BYTE), 1, BackupToFilePointer) < 1))break;
+					
+					/* Close file */
+					fclose(BackupFromFilePointer);
+					fclose(BackupToFilePointer);
+				}
+			}
+		}
+		else
+		{
+			/* Open file */
+			BackupFromFilePointer	= fopen(from,	"rb");
+			BackupToFilePointer		= fopen(to,		"wb");
+			if((BackupFromFilePointer == NULL) || (BackupToFilePointer == NULL))
+			{
+				fprintf(stderr, "Error: Cannot open file.\n");
+				continue;
+			}
+		
+			while(1)
+				if((fread(&b, sizeof(BYTE), 1, BackupFromFilePointer) < 1) || (fwrite(&b, sizeof(BYTE), 1, BackupToFilePointer) < 1))break;
+			
+			/* Close file */
+			fclose(BackupFromFilePointer);
+			fclose(BackupToFilePointer);
+		}
 	}
 	
 	/* Close file */
